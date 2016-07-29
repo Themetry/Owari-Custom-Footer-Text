7a970f107a97b196772c650ca8dec1512e1dd08c
Leland Fiegel <leland@Lelands-iMac.local>
2016-07-29 19:47:31 -0400

    Functional child theme

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Owari Custom Footer Text functions
+ *
+ * @package Owari Custom Footer Text
+ */
+
+/**
+ * Enqueue styles.
+ */
+function owari_custom_footer_text_styles() {
+	// Load the parent theme stylesheet
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+} // end function owari_custom_footer_text_styles
+add_action( 'wp_enqueue_scripts', 'owari_custom_footer_text_styles' );
